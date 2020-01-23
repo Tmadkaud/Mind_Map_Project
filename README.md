242174f08978689937929672f74f71a952939c9f
titi <thierry.madkaud@gmail.com>
2020-01-23 09:04:52 -0500

    more tests

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ Ce projet a pour but de concevoir facilement une mind map.
 
 - Vagrant
 - VirtualBox
-- Root Access (for packages installation)
-- Debian or Redhat or Centos based distribution
+- Root Access (pour l'installation des packages)
+- Distribution : Debian, Redhat ou Centos
 
 ## How to run
 
-In order to install it, just open a terminal and copy/paste the following commands:
+Afin d'installer et lancer l'API, ouvrez un terminal et copier/coller les commandes suivantes:
 
 ```
 git clone https://github.com/Tmadkaud/Mind_Map_Project.git
@@ -49,25 +49,21 @@ Pour supporter l'API, j'ai choisi de construire un environnement conteneur avec 
 
 J'ai choisi d'utiliser docker pour mes deux stack (flask et mongo)
 
-flask utilise une image légère de python 3, ainsi 
-
-J'ai creer une image docker via un dockerfile customiser (image python et des lib a utiliser qui sont les requirements)
+flask utilise une image légère de python 3, ainsi j'ai creer une image docker via un dockerfile customiser (image python et des lib a utiliser qui sont les requirements)
 
 pour mongodb j'ai juste utiliser l'image de base
 
 ### Pipeline.sh
 Ces deux containers sont managés par docker compose qui me permet de construire et de faire fonctionner les deux environnements ensemble en exposant leurs ports et en montant des volumes spécifiques
 
-J'ai voulu essayer de faire le plus de bonus point possible tout en simplifiant au maximum le deploiement et voulant minimiser le nombre d'opérations manuelles (setter les credatials AWS).
+J'ai voulu essayer de faire le plus de point bonus possible tout en simplifiant au maximum le deploiement et voulant minimiser le nombre d'opérations manuelles (setter les credentials AWS).
 
 Ainsi pour montrer ma passion de l'automatisation j'ai pu en un seul BASH script :
 
 - Provisionner une machine virtuelle vagrant, ayant pour but de simuler une instance AWS EC2. Toutes les prochaines étapes seront exécutées dans cette même instance.
 - Builder la Dockerfile pour l'environement Flask
 - Executer le dockercompose.yml
-- Executer differents test sur l API
+- Executer differents test sur l'API
 - Cleaner l'environement
 
 Tout cela me permet rapidement de simuler un deploiement, des test et la pereniter du script from scratch
-
-
--- pipeline.sh
+++ pipeline.sh
@@ -8,10 +8,19 @@ test_env() {
 	test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"map_id\\\": \\\"bar\\\"}' localhost/api/maps" "Create correct Map" 500 
 	test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"map_id\\\": \\\"bar\\\"}' localhost/api/maps" "Test create same Map" 500 
 	#Test leaf
-	test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"path\\\": \\\"I/live/nbc\\\", \\\"text\\\": \\\"hello nbc\\\"' localhost/api/maps/bar" "Create Leaf into bar map" 500 
+	test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"path\\\": \\\"I/live/nbc\\\", \\\"text\\\": \\\"hello nbc\\\"}' localhost/api/maps/bar" "Create Leaf into bar map" 500 
+        test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"path\\\": \\\"I/live/nbc\\\", \\\"text\\\": \\\"hello nbc\\\"}' localhost/api/maps/bar" "Test create same Leaf into bar map" 500
+        test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"path\\\": \\\"I/live/nbc\\\", \\\"text\\\": \\\"hello nbc\\\", \\\"other_param\\\": \\\"foo\\\"}' localhost/api/maps/bar" "Test multilple parameter for leaf" 500
+        test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"path\\\": \\\"I/live/nbc\\\", \\\"tex\\\": \\\"hello nbc\\\"}' localhost/api/maps/bar" "Test parameter test for leaf" 500
+        test_call "-X POST -H 'Content-Type: application/json' -d '{\\\"pat\\\": \\\"I/live/nbc\\\", \\\"text\\\": \\\"hello nbc\\\"}' localhost/api/maps/bar" "Test parameter path for leaf" 500
 
-
-
+        #Test display tree
+        test_call "localhost/api/maps/barr" "Test display a wrong Tree" 500
+        test_call "localhost/api/maps/bar" "Test display Tree" 200
+        
+        #Test get map
+        test_call "localhost/api/maps/bar/II/live/nbc" "Test get a wrong Map" 500
+        test_call "localhost/api/maps/bar/I/live/nbc" "Test get Map" 200
 }
 
 test_call() {
@@ -38,7 +47,8 @@ test_call() {
 		exit 1
 	fi
 }
-
+#test_env
+#exit 0
 
 is_virtualbox_installed=$(which virtualbox)
 if [[ $? -ne 0 ]]; then
@@ -60,6 +70,7 @@ else
 		else	
 			echo -e "\033[37m \nENVIRONMENT BUILD SUCCESS\033[0m\n"
 			test_env
+                        #exit 0
 			echo -e "\033[1m\n======== CLEAN ENVIRONEMENT ========\033[0m\n"
                         vagrant destroy -f
 			exit 0
